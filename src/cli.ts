a3ce03175635c5c3e5b21baab6af26c9921ae8ee
Hudson Silva Borges <hudsonsilbor@gmail.com>
2025-10-18 19:23:55 -0400

    chore: ignora ordenação temporariamente

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,23 @@
 import { Command, Option } from 'commander';
 import consola from 'consola';
 import path from 'path';
+import { knex } from './knex.js';
 import { download } from './lib/download.js';
+import { store } from './lib/store.js';
 import { uncompress } from './lib/uncompress.js';
 import env from './utils/env.js';
+
+function checkPAUFYYMM(PAUFYYMM: string): void {
+  const match = /^PA([A-Z]{2})(\d{2})(\d{2})$/.exec(PAUFYYMM);
+
+  if (!match) {
+    throw new Error('Invalid file. Use format PAUFYYMM (e.g. PAMS2501)');
+  } else if (parseInt(match[2]) < 8) {
+    throw new Error('Data before 2008 is not available for download.');
+  } else if (parseInt(match[3]) < 1 || parseInt(match[3]) > 12) {
+    throw new Error('Invalid month in PAUFYYMM. Month should be between 01 and 12.');
+  }
+}
 
 const program = new Command();
 
@@ -12,48 +26,63 @@ program.name('datasus').description('Utilities to work with DATASUS SIASUS files
 
 // download command
 program
-  .command('download <yymm>')
-  .description('Download a DATASUS SIASUS .dbc file given a YYMM (e.g. 2401)')
+  .command('download <PAUFYYMM>')
+  .description('Download a DATASUS SIASUS .dbc file given a PAUFYYMM (e.g. PAMS2501)')
   .addOption(
     new Option('--workdir <dir>', 'working directory').default(env.PET_WORKDIR).env('PET_WORKDIR'),
   )
   .addOption(new Option('--force', 'override file if it exists').default(false))
-  .action(async (yymm: string, options: { workdir: string; force: boolean }) => {
-    if (!yymm || !/^\d{4}$/.test(yymm)) {
-      consola.error('Invalid date. Use format YYMM (e.g. 2401)');
-      process.exit(2);
-    }
+  .action(async (PAUFYYMM: string, options: { workdir: string; force: boolean }) => {
+    checkPAUFYYMM(PAUFYYMM);
 
     const destDir = path.resolve(process.cwd(), options.workdir);
 
-    consola.start(`Downloading PAMS${yymm}.dbc to ${destDir}...`);
-    await download(`PAMS${yymm}.dbc`, destDir);
-    consola.success(`Downloaded PAMS${yymm}.dbc to ${destDir}`);
+    consola.start(`Downloading ${PAUFYYMM}.dbc to ${destDir}...`);
+    await download(`${PAUFYYMM}.dbc`, destDir);
+    consola.success(`Downloaded ${PAUFYYMM}.dbc to ${destDir}`);
   });
 
-// uncompress command (TODO)
+// uncompress command
 program
-  .command('uncompress <yymm>')
+  .command('uncompress <PAUFYYMM>')
   .description('Uncompress .dbc file (.dbc file should be downloaded)')
   .addOption(
     new Option('--workdir <dir>', 'working directory').default(env.PET_WORKDIR).env('PET_WORKDIR'),
   )
   .addOption(new Option('-f, --force', 'force reload / overwrite').default(false))
-  .action(async (yymm: string, options: { workdir: string; force: boolean }) => {
-    if (!yymm || !/^\d{4}$/.test(yymm)) {
-      consola.error('Invalid date. Use format YYMM (e.g. 2401)');
-      process.exit(2);
-    }
+  .action(async (PAUFYYMM: string, options: { workdir: string; force: boolean }) => {
+    checkPAUFYYMM(PAUFYYMM);
 
     const destDir = path.resolve(process.cwd(), options.workdir);
 
-    consola.start(`Uncompressing PAMS${yymm}.dbc in ${destDir}...`);
-    await uncompress(path.resolve(destDir, `PAMS${yymm}.dbc`));
-    consola.success(`Uncompressed PAMS${yymm}.dbc in ${destDir}`);
+    consola.start(`Uncompressing ${PAUFYYMM}.dbc in ${destDir}...`);
+    await uncompress(path.resolve(destDir, `${PAUFYYMM}.dbc`));
+    consola.success(`Uncompressed ${PAUFYYMM}.dbc in ${destDir}`);
+  });
+
+// store command
+program
+  .command('store <PAUFYYMM>')
+  .description('Store data from .dbc file into database (.dbc file should be uncompressed)')
+  .addOption(
+    new Option('--workdir <dir>', 'working directory').default(env.PET_WORKDIR).env('PET_WORKDIR'),
+  )
+  .addOption(new Option('-f, --force', 'force reload / overwrite').default(false))
+  .action(async (PAUFYYMM: string, options: { workdir: string; force: boolean }) => {
+    checkPAUFYYMM(PAUFYYMM);
+
+    const destDir = path.resolve(process.cwd(), options.workdir);
+
+    consola.start(`Storing data from ${PAUFYYMM}.dbc in ${destDir}...`);
+    await store(path.resolve(destDir, `${PAUFYYMM}.dbf`));
+    consola.success(`Stored data from ${PAUFYYMM}.dbc in ${destDir}`);
   });
 
 // parse and run commands
-program.parseAsync(process.argv).catch((err) => {
-  consola.error('Error executing command:', err instanceof Error ? err.message : err);
-  process.exit(1);
-});
+program
+  .parseAsync(process.argv)
+  .catch((err) => {
+    consola.error('Error executing command:', err instanceof Error ? err.message : err);
+    process.exit(1);
+  })
+  .finally(() => knex.destroy());
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,5 +3,10 @@ import z from 'zod';
 export default z
   .object({
     PET_WORKDIR: z.string().default('./data'),
+    PET_DB_HOST: z.string().default('localhost'),
+    PET_DB_PORT: z.coerce.number().default(5432),
+    PET_DB_USER: z.string().default('postgres'),
+    PET_DB_PASSWORD: z.string().default('password'),
+    PET_DB_NAME: z.string().default('datasus'),
   })
   .parse(process.env);
